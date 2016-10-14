05297692eddee03c859093b6b7bf931f584de871
JustOff <Off.Just.Off@gmail.com>
2016-10-14 16:11:15 +0300

    cookexterm: 2.6.1b1 new LocalStorage cleanup algo

--- content/crusher.js
+++ content/crusher.js
@@ -1,10 +1,18 @@
 let EXPORTED_SYMBOLS = ["Crusher"];
 
+let Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
+
 Components.utils.import("resource://gre/modules/Services.jsm");
+
+let ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+let securityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
+let domStorageManager = Cc["@mozilla.org/dom/storagemanager;1"].getService(Ci.nsIDOMStorageManager);
 
 let Crusher = function(Prefs, Buttons, Whitelist, Log, Notifications, Utils) {
 	let Crusher = this;
 
+	this.storageTracker = {};
+	
 	this.handleCookieChanged = function(aSubject, aTopic, aData) {
 		switch (aData) {
 			case "added":
@@ -15,22 +23,15 @@ let Crusher = function(Prefs, Buttons, Whitelist, Log, Notifications, Utils) {
 	};
 
 	this.handleDomStorageChanged = function(aSubject, aTopic, aData) {
-		let de = aSubject;
-		try {
-			de = de.QueryInterface(Components.interfaces.nsIDOMStorageEvent);
-		} catch(e) {
-			// ignore
-		}
-
-		// ignore entries that just changed
-		if (de.newValue && de.oldValue) return;
-
-		if (de.oldValue != null && de.newValue == null) {
-		// entry removed
-		// ignored for now
-		} else {
-			// entry added
-			Crusher.prepareStorage(de.url);
+		if (aTopic == "dom-storage2-changed") {
+			if (aSubject.key == null) {
+				// cleared
+			} else if (aSubject.oldValue == null) {
+				// added
+				let uri = ioService.newURI(aSubject.url, null, null);
+				Crusher.storageTracker[uri.host] = true;
+Components.utils.reportError("[+s] " + uri.host);
+			}
 		}
 	}
 	
@@ -45,19 +46,18 @@ let Crusher = function(Prefs, Buttons, Whitelist, Log, Notifications, Utils) {
 		}
 	};
 
-this.jobID = 0;
-
-	this.prepareStorage = function(url) {
+	this.prepareStorage = function(host) {
 		if (!Prefs.getValue("suspendCrushing") && Prefs.getValue("keepCrushingLocalStorage")) {
-			if (url === true) {
+			if (host === true) {
 				this.executeStorage(true);
 			} else {
-if (url) { Components.utils.reportError("[+s] 4" + url); }
-				Utils.setTimeout(this.executeStorage.bind(this, url), Prefs.getValue("crushingDelay"));
+				Utils.setTimeout(this.executeStorage.bind(this, host), Prefs.getValue("crushingDelay"));
 			}
 		}
 	}
 	
+this.jobID = 0;
+
 	this.execute = function(onecookie) {
 		let cookies = [];
 		let crushedCookiesDomains = {};
@@ -77,6 +77,7 @@ this.jobID++;
 
 		for (let cookie of cookies) {
 			if (this.mayBeCrushed(cookie, cleanup)) {
+//Components.utils.reportError("oA: " + typeof cookie.originAttributes);
 				if (typeof cookie.originAttributes === "object") {
 					Services.cookies.remove(cookie.host, cookie.name, cookie.path, false, cookie.originAttributes);
 				} else {
@@ -144,19 +145,6 @@ this.jobID++;
 							cookie.isDomain && cookie.rawHost == domain.substring(domain.indexOf(".") + 1)) {
 						return false;
 					}
-/*
-					if (Prefs.getValue("keepCrushingLocalStorage")) {
-						let storage;
-						try {
-							storage = browser.contentWindow.localStorage;
-						} catch(e) {}
-
-						if (storage) {
-Components.utils.reportError("[" + this.jobID + "][-SC] " + domain + "(" + cookie.rawHost + ")");
-							storage.clear();
-						}
-					}
-*/
 				}
 			}
 		}
@@ -166,12 +154,108 @@ Components.utils.reportError("[" + this.jobID + "][-SC] " + domain + "(" + cooki
 	
 this.jobIDs = 0;
 
-	this.executeStorage = function(url) {
-		let cleanup = url === true;
+	this.executeStorage = function(onehost) {
+		let crushedStorageDomains = {};
+		let crushedSomething = false;
+
+		let cleanup = onehost === true;
 this.jobIDs++;
 
-		if (!cleanup && url) {
-Components.utils.reportError("[" + this.jobIDs + "s][*] " + url);
+//		if (cleanup) {
+//		} else {
+			for (let host in this.storageTracker) {
+				if (this.mayBeCrushedStorage(host, cleanup)) {
+					clearStorage(host, "http", 80);
+					clearStorage(host, "https", 443);
+					delete this.storageTracker[host];
+					crushedStorageDomains[host] = true;
+					crushedSomething = true;
+Components.utils.reportError("[" + this.jobIDs + "s][-] " + host);
+				} else {
+Components.utils.reportError("[" + this.jobIDs + "s][*] " + host);
+				}
+			}
+//		}
+
+		if (crushedSomething) {
+			let crushedStorageDomainsString = "";
+
+			for (let domain in crushedStorageDomains) {
+				crushedStorageDomainsString += domain + ", ";
+			}
+
+			crushedStorageDomainsString = crushedStorageDomainsString.slice(0, -2);
+
+			Buttons.notify(crushedStorageDomainsString);
+			Notifications.notify(crushedStorageDomainsString);
+			Log.log(crushedStorageDomainsString); 
+		} else {
+			Buttons.notify();
 		}
-	}
+	};
+
+	this.mayBeCrushedStorage = function(host, cleanup) {
+		if (Whitelist.isWhitelisted(host)) {
+			return false;
+		}
+
+		if (cleanup) {
+			return true;
+		}
+
+		if (Whitelist.isWhitelistedTemp(host)) {
+			return false;
+		}
+
+		let windowsEnumerator = Services.wm.getEnumerator("navigator:browser");
+
+		while (windowsEnumerator.hasMoreElements()) {
+			let window = windowsEnumerator.getNext().QueryInterface(Components.interfaces.nsIDOMWindow);
+			let tabBrowser = window.gBrowser;
+
+			for (let browser of tabBrowser.browsers) {
+				let domain;
+				try {
+					domain = browser.contentDocument.domain;
+				} catch(e) {}
+
+				if (domain) {
+					domain = Utils.UTF8toACE(domain);
+
+					if (host == domain) {
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
+	};
 };
+
+function clearStorage(dom, proto, port) {
+//  try {
+      var storage = getLocalStorage(dom, proto, port);
+      if (storage) {
+        storage.clear();
+      }
+//  } catch(e) {
+//Components.utils.reportError(e.message);
+//  }
+}
+
+function getLocalStorage(domain, proto, port) {
+  var uri, principal, storage;
+  var s = proto + "://" + (domain.startsWith(".") ? domain.substr(1) : domain) + ":" + (port == -1 ? 80 : port);
+  try {
+    uri = ioService.newURI(s, null, null);
+    principal = securityManager.getNoAppCodebasePrincipal(uri);
+    storage = domStorageManager.getLocalStorageForPrincipal(principal, null);
+  } catch (e) {
+Components.utils.reportError("localstorage: ", e.message);
+    return null;
+  }
+
+  if (!storage || storage.length < 1) return null;
+  return storage;
+}
