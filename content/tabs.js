0650b396be3e826c75cd7f5f069e60b6d01c96f4
JustOff <Off.Just.Off@gmail.com>
2016-10-25 22:09:11 +0300

    cookexterm: no need to set aBrowser["previousDomain"] = "undefined" for all browsers in all tabs on window close

--- content/tabs.js
+++ content/tabs.js
@@ -3,7 +3,6 @@ let EXPORTED_SYMBOLS = ["Tabs"];
 let Tabs = function(Cleaner, Buttons, Utils) {
 	this.onClose = function(event) {
 		let domain = Utils.getHostFromTab(event.target);
-
 		if (domain) {
 			Cleaner.prepare();
 		}
@@ -21,7 +20,6 @@ let Tabs = function(Cleaner, Buttons, Utils) {
 					if (previousDomain && previousDomain != domain) {
 						Cleaner.prepare();
 					}
-
 					aBrowser["previousDomain"] = domain;
 				}
 			} catch(e) {}
@@ -33,7 +31,6 @@ let Tabs = function(Cleaner, Buttons, Utils) {
 			if (aFlag & Components.interfaces.nsIWebProgress.LOCATION_CHANGE_SAME_DOCUMENT) {
 				return;
 			}
-
 			try {
 				Buttons.refresh();
 			} catch(e) {}
@@ -42,7 +39,6 @@ let Tabs = function(Cleaner, Buttons, Utils) {
 
 	this.init = function(window) {
 		let tabBrowser = window.gBrowser;
-
 		tabBrowser.tabContainer.addEventListener("TabClose", this.onClose, false);
 		tabBrowser.addTabsProgressListener(this.onTabProgress);
 		tabBrowser.addProgressListener(this.onProgress);
@@ -50,12 +46,6 @@ let Tabs = function(Cleaner, Buttons, Utils) {
 
 	this.clear = function(window) {
 		let tabBrowser = window.gBrowser;
-
-		for (let tab of tabBrowser.tabs) {
-			let browser = window.gBrowser.getBrowserForTab(tab);
-			browser["previousDomain"] = undefined;
-		}
-
 		tabBrowser.tabContainer.removeEventListener("TabClose", this.onClose, false);
 		tabBrowser.removeTabsProgressListener(this.onTabProgress);
 		tabBrowser.removeProgressListener(this.onProgress);
