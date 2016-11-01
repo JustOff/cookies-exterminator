2aca50c37c140a9832ec0927b7d5e0dad82dadef
JustOff <Off.Just.Off@gmail.com>
2016-11-01 23:20:32 +0200

    cookexterm: move windows.js logic to bootstrap.js, rewrite & fix windows handling, remove windows.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,7 @@
 let Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 
 const extName = "cookies-xtrm";
 const extJSPath = "chrome://" + extName + "/content/";
@@ -11,14 +12,50 @@ const INCOMPATIBLE = {
 	"john%40velvetcache.org": "Beef Taco"
 };
 
-let Prefs = null;
-let Log = null;
-let Cleaner = null;
-let Windows = null;
-
-let onPrefsEvent = null;
-let appInfo = null;
+let Prefs, Log, Buttons, Cleaner, Tabs, gWindowListener, onPrefsEvent, appInfo;
 let compat = true;
+
+function browserWindowObserver(handlers) {
+	this.handlers = handlers;
+}
+
+browserWindowObserver.prototype = {
+	observe: function(aSubject, aTopic, aData) {
+		if (aTopic == "domwindowopened") {
+			aSubject.QueryInterface(Ci.nsIDOMWindow).addEventListener("load", this, false);
+		} else if (aTopic == "domwindowclosed") {
+			if (aSubject.document.documentElement.getAttribute("windowtype") == "navigator:browser") {
+				this.handlers.onShutdown(aSubject);
+			}
+		}
+	},
+	handleEvent: function(aEvent) {
+		let aWindow = aEvent.currentTarget;
+		aWindow.removeEventListener(aEvent.type, this, false);
+
+		if (aWindow.document.documentElement.getAttribute("windowtype") == "navigator:browser") {
+			this.handlers.onStartup(aWindow);
+		}
+	}
+};
+
+function browserWindowStartup(aWindow) {
+	if (!PrivateBrowsingUtils.isWindowPrivate(aWindow)) {
+		Tabs.init(aWindow);
+		Buttons.init(aWindow);
+	}
+}
+
+function browserWindowShutdown(aWindow) {
+	if (!PrivateBrowsingUtils.isWindowPrivate(aWindow)) {
+		Tabs.clear(aWindow);
+		Buttons.clear(aWindow);
+		let windowsEnumerator = Services.wm.getEnumerator("navigator:browser");
+		if (windowsEnumerator.hasMoreElements()) {
+			Cleaner.prepare();
+		}
+	}
+}
 
 function startup(data, reason) {
 	appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
@@ -41,7 +78,6 @@ function startup(data, reason) {
 	Cu.import(extJSPath + "log.js", Imports);
 	Cu.import(extJSPath + "cleaner.js", Imports);
 	Cu.import(extJSPath + "tabs.js", Imports);
-	Cu.import(extJSPath + "windows.js", Imports);
 	Cu.import(extJSPath + "notifications.js", Imports);
 	Cu.import(extJSPath + "utils.js", Imports);
 
@@ -49,11 +85,10 @@ function startup(data, reason) {
 	Prefs = new Imports.Prefs(extName, appInfo, Utils);
 	let Notifications = new Imports.Notifications(extName, Prefs, Utils);
 	let Whitelist = new Imports.Whitelist(Prefs, Notifications);
-	let Buttons = new Imports.Buttons(extName, appInfo, Prefs, Whitelist, Utils);
+	Buttons = new Imports.Buttons(extName, appInfo, Prefs, Whitelist, Utils);
 	Log = new Imports.Log(Prefs, Utils);
 	Cleaner = new Imports.Cleaner(Prefs, Buttons, Whitelist, Log, Notifications, Utils);
-	let Tabs = new Imports.Tabs(Cleaner, Buttons, Utils);
-	Windows = new Imports.Windows(Tabs, Buttons, Cleaner, Prefs);
+	Tabs = new Imports.Tabs(Cleaner, Buttons, Utils);
 
 	Prefs.init();
 	if (reason == ADDON_INSTALL) {
@@ -78,7 +113,6 @@ function startup(data, reason) {
 
 	Whitelist.init();
 	Utils.setTimeout(Cleaner.getScopesFromDB.bind(this, null), 10);
-	Windows.init();
 
 	onPrefsEvent = {
 		observe: function(aSubject, aTopic, aData) {
@@ -100,6 +134,17 @@ function startup(data, reason) {
 	Services.obs.addObserver(Log.onEvent, "cookextermLogEvent", false);
 	Services.obs.addObserver(Cleaner.handleCookieChanged, "cookie-changed", false);
 	Services.obs.addObserver(Cleaner.handleDomStorageChanged, "dom-storage2-changed", false);
+
+	gWindowListener = new browserWindowObserver({
+		onStartup: browserWindowStartup,
+		onShutdown: browserWindowShutdown
+	});
+	Services.ww.registerNotification(gWindowListener);
+
+	let winenu = Services.wm.getEnumerator("navigator:browser");
+	while (winenu.hasMoreElements()) {
+		browserWindowStartup(winenu.getNext());
+	}
 }
 
 function shutdown(data, reason) {
@@ -114,10 +159,15 @@ function shutdown(data, reason) {
 
 	if (compat) {
 		if(reason == APP_SHUTDOWN) {
-			Windows.clear(true);
+			Cleaner.prepare("Cleanup");
 			return;
-		} else {
-			Windows.clear();
+		}
+
+		Services.ww.unregisterNotification(gWindowListener);
+
+		let winenu = Services.wm.getEnumerator("navigator:browser");
+		while (winenu.hasMoreElements()) {
+			browserWindowShutdown(winenu.getNext());
 		}
 
 		Services.obs.removeObserver(onPrefsEvent, "cookextermPrefsEvent");
@@ -132,7 +182,6 @@ function shutdown(data, reason) {
 	Cu.unload(extJSPath + "log.js");
 	Cu.unload(extJSPath + "cleaner.js");
 	Cu.unload(extJSPath + "tabs.js");
-	Cu.unload(extJSPath + "windows.js");
 	Cu.unload(extJSPath + "notifications.js");
 	Cu.unload(extJSPath + "utils.js");
 }
