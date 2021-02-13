c1031e8527ac4ae52d7cc2673369a7dacb348c28
JustOff <Off.Just.Off@gmail.com>
2021-02-13 23:02:34 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=2.9.6
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/cookexterm-.+?\.xpi/download\/%VER%\/cookexterm-%VER%\.xpi/" update.xml
+
+set XPI=cookexterm-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
